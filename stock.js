7695f9c26cf09fc94bda7fdc3fa4e82f22107445
danielpu23 <70337232+danielpu23@users.noreply.github.com>
2021-11-20 14:43:54 -0800

    Update stock.js

--- stock.js
+++ stock.js
@@ -9,29 +9,35 @@ var prompt = require('prompt-sync')();
 console.log("This program will list a stock's details for a given date within the past 5 months");
 
 var stockName = "";
-var readStock = true;
-while (readStock) {
+var continueReading = true;
+while (continueReading) {
     stockName = prompt("Enter a stock symbol: ");
+    continueReading = readStock(stockName);
+}
+
+console.log("Program has exited successfully");
+
+function isCharNumber(c) {    // check if given char is number
+    return c >= '0' && c <= '9';
+}
+
+function readStock(stockName) {
 
     var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + stockName + '&apikey=' + process.env.KEY;
-
     var stockDate = "";
     stockDate = prompt("Enter a date MM/DD/YYYY: ");
     if (stockDate.length != 10 || !isCharNumber(stockDate[0]) || !isCharNumber(stockDate[1]) || stockDate[2] != '/' ||     // make sure the date is formatted correctly
         !isCharNumber(stockDate[3]) || !isCharNumber(stockDate[4]) || stockDate[5] != '/' || !isCharNumber(stockDate[6]) ||
         !isCharNumber(stockDate[7]) || !isCharNumber(stockDate[8]) || !isCharNumber(stockDate[9])) {
         console.log("Incorrect date format");
-        readStock = false;
     }
     else {
         request.get({ url: url, json: true, headers: { 'User-Agent': 'request' } }, (err, res, data) => {   // read stock data from API and make sure no errors occur
             if (err) {
                 console.log('Error:', err);
-                readStock = false;
             }
             else if (res.statusCode !== 200) {
                 console.log('Status:', res.statusCode);
-                readStock = false;
             }
             else {
                 var stockData = data['Time Series (Daily)'][stockDate.substring(6, 10)
@@ -39,13 +45,13 @@ while (readStock) {
                     stockDate.substring(3, 5)];
                 if (stockData == undefined) {
                     console.log("Date out of range");
-                    readStock = false;
                 }
                 else {
                     console.log(stockData);
+                    console.log('\n');
                     var read = prompt("Continue reading stock data? Y/N: ");
                     if (read.toLowerCase().replace(/\s/g, '') == "n") {
-                        readStock = false;
+                        return false;
                     }
                     else if (read.toLowerCase().replace(/\s/g, '') != "y" || read.toLowerCase().replace(/\s/g, '') != "n") {
                         var incorrectFormat = true;
@@ -53,11 +59,10 @@ while (readStock) {
                             console.log("Incorrect answer format, expected Y or N");
                             read = prompt("Continue reading stock data? Y/N: ");
                             if (read.toLowerCase().replace(/\s/g, '') == "n") {
-                                readStock = false;
-                                incorrectFormat = false;
+                                return false;
                             }
                             else if (read.toLowerCase().replace(/\s/g, '') == "y") {
-                                readStock = false;
+                                incorrectFormat = false;
                             }
                         }
                     }
@@ -65,8 +70,5 @@ while (readStock) {
             }
         });
     }
-}
-
-function isCharNumber(c) {    // check if given char is number
-    return c >= '0' && c <= '9';
+    return true;
 }
